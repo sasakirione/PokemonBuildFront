15419f3d08983a07dad4dcf9c92906610c682657
sasakirione <sasakirione@gmail.com>
2022-09-26 01:17:42 +0900

    Update: 情報を微修正2

--- pages/information.tsx
+++ pages/information.tsx
@@ -4,7 +4,6 @@ import React, {useState} from "react";
 import {
     Button,
     Card,
-    CardActionArea,
     CardContent,
     Dialog,
     DialogContent,
@@ -31,60 +30,69 @@ const Information: NextPage = () => {
 
     return (<>
         <PokeBuildHead title="インフォメーション"/>
-        <Grid container spacing={2} direction="row" justifyContent="space-evenly" alignItems="stretch">
+        <Grid container spacing={1} direction="row" justifyContent="space-evenly" alignItems="stretch">
             <Grid item xs={12} lg={6}>
                 <Card className="info-card" variant="outlined">
-                    <Typography variant="h5" component="div" className="card-title">
-                        更新履歴
-                    </Typography>
-                    <List>
-                        {historyList.slice(0, 4).map((history) => (
-                            <ListItem key={history.date}>
-                                <ListItemText primary={history.date} secondary={history.content}/>
+                    <CardContent>
+                        <Typography variant="h5" component="div">
+                            更新履歴
+                        </Typography>
+                        <List>
+                            {historyList.slice(0, 4).map((history) => (
+                                <ListItem key={history.date}>
+                                    <ListItemText primary={history.date} secondary={history.content}/>
+                                </ListItem>
+                            ))}
+                            <ListItem><Button onClick={handleOpenHistoryDetail}>more</Button></ListItem>
+                        </List>
+                    </CardContent>
+                </Card>
+            </Grid>
+            <Grid item xs={12} lg={6}>
+                <Card className="info-card" variant="outlined">
+                    <CardContent>
+                        <Typography variant="h5" component="div">
+                            使用方法
+                        </Typography>
+                        <List>
+                            <ListItem>1. 右上のログインボタンからログインを行う</ListItem>
+                            <ListItem>2. 構築画面が表示されたら右上のポケモンを追加ボタンを押す</ListItem>
+                            <ListItem>3. 必要項目を入力しOKを押すとポケモンが保存されます</ListItem>
+                            <ListItem>4. 登録されたポケモンは該当の項目をクリックすると編集画面がでます</ListItem>
+                        </List>
+                    </CardContent>
+                </Card>
+            </Grid>
+            <Grid item xs={12} lg={6}>
+                <Card className="info-card" variant="outlined">
+                    <CardContent>
+                        <Typography variant="h5" component="div">
+                            おことわり
+                        </Typography>
+                        <List>
+                            <ListItem>
+                                本ツールはポケモン公式とは一切関係のないファンメイドです。
                             </ListItem>
-                        ))}
-                    </List>
-                    <CardActionArea>
-                        <Button onClick={handleOpenHistoryDetail}>more</Button>
-                    </CardActionArea>
-                </Card>
-            </Grid>
-            <Grid item xs={12} lg={6}>
-                <Card className="info-card" variant="outlined">
-                    <Typography variant="h5" component="div" className="card-title">
-                        使用方法
-                    </Typography>
-                    <CardContent>
-                        1. 右上のログインボタンからログインを行う<br/>
-                        2. 構築画面が表示されたら右上のポケモンを追加ボタンを押す<br/>
-                        3. 必要項目を入力しOKを押すと保存されます<br/>
-                        4. 登録された構築情報は該当の項目をクリックすると編集画面がでます<br/>
+                            <ListItem>
+                                様々な計算値について可能な限り正確な値が出来るように設計していますが、完全に正しい結果を保証するわけではありません。
+                                ランクマッチでの対戦結果を含めて本ツールを使用する事によって生じたいかなる損害も開発者は責任を負いかねます。
+                            </ListItem>
+                        </List>
                     </CardContent>
                 </Card>
             </Grid>
             <Grid item xs={12} lg={6}>
                 <Card className="info-card" variant="outlined">
-                    <Typography variant="h5" component="div" className="card-title">
-                        おことわり
-                    </Typography>
                     <CardContent>
-                        <div>・本ツールはポケモン公式とは一切関係のないファンサイトです。</div>
-                        <div>・様々な計算値について可能な限り正確な値が出来るように設計していますが、完全に正しい結果を保証するわけではありません。
-                            ランクマッチでの対戦結果を含めて本ツールを使用する事によって生じたいかなる損害も開発者は責任を負いかねます。
-                        </div>
-                    </CardContent>
-                </Card>
-            </Grid>
-            <Grid item xs={12} lg={6}>
-                <Card className="info-card" variant="outlined">
-                    <Typography variant="h5" component="div" className="card-title">
-                        技術スタック
-                    </Typography>
-                    <CardContent>
+                        <Typography variant="h5" component="div">
+                            技術スタック
+                        </Typography>
                         <List>
                             <ListItem>フロントエンド： TypeScript, Next.js, Material-UI</ListItem>
                             <ListItem>バックエンド： Kotlin, Ktor, Exposed, PostgreSQL</ListItem>
-                            <ListItem>クラウド： Auth0, Cloud SQL, Cloud Build, Cloud Run</ListItem>
+                            <ListItem>外部サービス： Auth0, Cloud SQL, Cloud Build, Cloud Run</ListItem>
+                            <ListItem>開発： IntelliJ, WebStorm, Docker, GitHub</ListItem>
+                            <ListItem>過去にお世話になったサービス： Heroku</ListItem>
                         </List>
                     </CardContent>
                 </Card>
