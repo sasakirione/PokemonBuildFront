91965cde7660280498c02edbab68222246124e07
sasakirione <sasakirione@gmail.com>
2022-10-03 21:42:36 +0900

    etc: バージョンアップ対応2

--- domain/history.ts
+++ domain/history.ts
@@ -1,6 +1,10 @@
 export const siteHistory = {
   "historyList": [
     {
+      "date": "2022-10-03",
+      "content": "一部ポケモンで覚える技のみの表示に対応"
+    },
+    {
       "date": "2022-09-27",
       "content": "UIや動作を改善、HP偶数警告を追加"
     },
