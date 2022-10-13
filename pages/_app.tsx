08371274798bcb13576a3dcfaa38a4061fe9af34
sasakirione <sasakirione@gmail.com>
2022-10-13 20:01:35 +0900

    Update: 通信エラー時の共通処理を定義
    ついでにデフォルトのfetcherも

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,9 @@ import Header from "../components/atomic/Header";
 import {Auth0Provider} from "@auth0/auth0-react";
 import {Footer} from "../components/atomic/Footer";
 import {PokemonConstProvider} from "../components/hook/PokemonConst";
-import {Toaster} from "react-hot-toast";
+import toast, {Toaster} from "react-hot-toast";
+import SWRConfig from "swr/dist/utils/config-context";
+import axios from "axios";
 
 function MyApp({Component, pageProps}: AppProps) {
     const redirectUrl = process.env.NEXT_PUBLIC_AUTH0_REDIRECT_URL!
@@ -17,13 +19,33 @@ function MyApp({Component, pageProps}: AppProps) {
             clientId={clientId}
             redirectUri={redirectUrl}
         >
-            <PokemonConstProvider>
-                <Header/>
-                <Component {...pageProps} />
-                <div className="footer-space"></div>
-                <Footer/>
-                <Toaster/>
-            </PokemonConstProvider>
+            <SWRConfig value={{
+                fetcher: (url: string) => axios.get(url).then(res => res.data),
+                onError: (err: any) => {
+                    if (err.status == 401 && err.status == 403) {
+                        toast.error("通信に失敗しました。認証系のエラーです。ログインをやり直してください。")
+                    } else if (err.status == 404) {
+                        toast.error("通信に失敗しました。不正な値が送信されました。")
+                    } else if (err.status == 500) {
+                        toast.error("通信に失敗しました。サーバー側のエラーです。@sasakirione2までお知らせください。")
+                    } else {
+                        toast.error("通信に失敗しました。")
+                    }
+                },
+                onErrorRetry: (error, key, config, revalidate, {retryCount}) => {
+                    if (error.status === 404) return
+                    if (retryCount >= 3) return
+                    setTimeout(() => revalidate({retryCount: retryCount + 1}), 1000)
+                }
+            }}>
+                <PokemonConstProvider>
+                    <Header/>
+                    <Component {...pageProps} />
+                    <div className="footer-space"></div>
+                    <Footer/>
+                    <Toaster/>
+                </PokemonConstProvider>
+            </SWRConfig>
         </Auth0Provider>
     )
 }
